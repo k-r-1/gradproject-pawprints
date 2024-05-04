47752e80d59138f4e4687b0cb1652d5d6e1ff12d
박주원 <juwon_0715@naver.com>
2024-05-04 17:33:13 +0900

    테스트

---
+++ springserver/pawprints/src/main/java/com/swuproject/pawprints/PawprintsApplication.java
@@ -0,0 +1,13 @@
+package com.swuproject.pawprints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawprintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawprintsApplication.class, args);
+	}
+
+}
---
+++ springserver/pawprints/src/test/java/com/swuproject/pawprints/PawprintsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swuproject.pawprints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawprintsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
