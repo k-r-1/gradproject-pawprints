627a86ba95883fbe8087d4e20007ef748ca455e0
이기연 <keilly0104@naver.com>
2024-05-04 17:45:27 +0900

    kiyeon test

--- springserver/pawprints/src/main/java/com/swuproject/pawprints/PawprintsApplication.java
+++ springserver/pawprints/src/main/java/com/swuproject/pawprints/PawprintsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PawprintsApplication {
-
+	// test
 	public static void main(String[] args) {
 		SpringApplication.run(PawprintsApplication.class, args);
 	}
