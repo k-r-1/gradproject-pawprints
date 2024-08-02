998a00023e942003910c4e5d6414fd06a79d6788
이기연 <keilly0104@naver.com>
2024-08-02 15:59:14 +0900

    feat.naver map 아래에 목격 신고 리스트 출력

--- androidapp/app/src/main/java/com/swuproject/pawprints/ui/map/MapFragment.kt
+++ androidapp/app/src/main/java/com/swuproject/pawprints/ui/map/MapFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
@@ -13,6 +15,12 @@ import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.LocationTrackingMode
 import com.swuproject.pawprints.R
 import com.swuproject.pawprints.databinding.FragmentMapBinding
+import com.swuproject.pawprints.dto.SightReportResponse
+import com.swuproject.pawprints.network.RetrofitClient
+import com.swuproject.pawprints.ui.home.SightRecyclerAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
@@ -29,7 +37,15 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         val mapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val rootView: View = binding.root
+
+        val recyclerView = rootView.findViewById<RecyclerView>(R.id.map_sightRecyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        fetchSightReports { sightReports ->
+            val adapter = SightRecyclerAdapter(sightReports, requireContext())
+            recyclerView.adapter = adapter
+        }
 
         // Initialize Naver Map
         val fm = childFragmentManager
@@ -43,7 +59,25 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         // Initialize FusedLocationSource
         locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
-        return root
+        return rootView
+    }
+
+    private fun fetchSightReports(callback: (List<SightReportResponse>) -> Unit) {
+        val retrofitService = RetrofitClient.getRetrofitService()
+        retrofitService.getSightReports().enqueue(object : Callback<List<SightReportResponse>> {
+            override fun onResponse(
+                call: Call<List<SightReportResponse>>,
+                response: Response<List<SightReportResponse>>
+            ) {
+                if (response.isSuccessful) {
+                    callback(response.body() ?: emptyList())
+                }
+            }
+
+            override fun onFailure(call: Call<List<SightReportResponse>>, t: Throwable) {
+                // 실패 처리
+            }
+        })
     }
 
     override fun onMapReady(naverMap: NaverMap) {
