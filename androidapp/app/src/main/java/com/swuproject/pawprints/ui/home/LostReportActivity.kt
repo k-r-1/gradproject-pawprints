2b036ab9b77aa679fc6dbb558da789a7f820aed9
박주원 <juwon_0715@naver.com>
2024-08-02 14:31:38 +0900

    feat: 홈 화면 플로팅 액션 버튼 클릭 시 실종 신고 및 목격 신고 액티비티로 이동 기능 추가

---
+++ androidapp/app/src/main/java/com/swuproject/pawprints/ui/home/LostReportActivity.kt
@@ -0,0 +1,21 @@
+package com.swuproject.pawprints.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.swuproject.pawprints.R
+
+class LostReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_lost_report)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
